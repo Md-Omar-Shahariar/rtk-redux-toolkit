1a326398ea2431b588f36cd175b84d3f8ddb48d0
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-04 14:47:26 +0600

    dynamicCounter

--- app/store.js
+++ app/store.js
@@ -1,8 +1,10 @@
 const { configureStore } = require("@reduxjs/toolkit");
 const counterReducer = require("../features/counter/counterSlice");
+const dynamicCounterReducer = require("../features/dynamicCounter/dynamicCounter");
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    dynamicCounter: dynamicCounterReducer,
   },
 });
 module.exports = store;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const store = require("./app/store");
 const { counterActions } = require("./features/counter/counterSlice");
+const {
+  dynamicCounterActions,
+} = require("./features/dynamicCounter/dynamicCounter");
 
 // subscribe to state changes
 console.log(`initialState ${JSON.stringify(store.getState())}`);
